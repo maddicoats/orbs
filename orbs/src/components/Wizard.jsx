23f23bd2c21f08dac79fdb4eabe0ba835009169b
maddicoats <mcoats799@gmail.com>
2022-11-16 22:30:56 +1030

    working on making wizard invisible when orb is clicked

--- orbs/src/components/Wizard.jsx
+++ orbs/src/components/Wizard.jsx
@@ -2,13 +2,39 @@ import React from 'react'
 import style from './Wizard.module.scss'
 import wizardPic from './../data/orb-wizard.png'
 import Orbs from './Orbs'
+import { useState, useEffect } from 'react'
 
 const Wizard = () => {
+ const [orbIsClicked, setOrbIsClicked] = useState(false);
+ const [wizardIsVisible, setWizardIsVisible] = useState(true)
+
+//  const wizardShroud = (event) => {
+//         if (!wizardIsVisible) {
+//             setWizardIsVisible(false)
+//         }
+//     }
+
+
   return (
     <div className={style.Wiz}>
         <h1>Pin the Orb on the Wizard</h1>
-        <img src={wizardPic} alt="a wizard pondering his orb" height={500} />
-        <Orbs />
+
+        <img 
+          src={wizardPic} 
+          alt="a wizard pondering his orb" 
+          height={500}
+          className={!orbIsClicked?style.Wizible:style.Inwizible}
+          onMouseOver={ () => {
+            setWizardIsVisible(false)
+          }}
+        />
+
+        <Orbs 
+          onMouseDown={ () => {
+            setOrbIsClicked(true)
+            console.log(orbIsClicked)
+          }}
+        />
     </div>
   )
 }
